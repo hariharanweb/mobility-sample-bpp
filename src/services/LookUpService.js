cd49b4fbf2b6114ee1a29438be94136cc5bba09f
priyajames29 <42799848+priyajames29@users.noreply.github.com>
2023-01-04 16:25:54 +0530

    Refactor. LookUpService

--- src/services/LookUpService.js
+++ src/services/LookUpService.js
@@ -1,17 +1,17 @@
-import Api from '../api/Api';
+import Api from "../api/Api";
 
 const REGISTRY_URL = `${process.env.GATEWAY_URL}/lookup`;
 
 const getPublicKey = async (type) => {
-    const request = JSON.stringify({
-      type: type,
-    });
-  
-    const response = await Api.doPost(REGISTRY_URL, request);
-    const responseJson = await response.json();
-    return responseJson[0].signing_public_key;
-  };
+  const request = JSON.stringify({
+    type: type,
+  });
 
-  export default {
-    getPublicKey
-  };
+  const response = await Api.doPost(REGISTRY_URL, request);
+  const responseJson = await response.json();
+  return responseJson[0].signing_public_key;
+};
+
+export default {
+  getPublicKey,
+};

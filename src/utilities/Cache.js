b13d8f85c540f58af98ff70130b1bb1524ecdab7
priyajames29 <42799848+priyajames29@users.noreply.github.com>
2023-01-05 17:40:09 +0530

    Refactor. Cache.js

--- src/utilities/Cache.js
+++ src/utilities/Cache.js
@@ -5,7 +5,7 @@ const cache = new NodeCache({ stdTTL: 200000, checkperiod: 200000 });
 const logger = LoggingService.getLogger('Cache');
 
 const getCache = (cachekey) => {
-  logger.debug('Inside get chache');
+  logger.debug(`Inside get chache, the public key is: ${cache.get(cachekey)}`);
   return cache.get(cachekey);
 };
 

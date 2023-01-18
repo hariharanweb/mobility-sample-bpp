3982221c6354185719a538011de1156779b09312
Japneet Kaur Bhatia <japneetk1111@gmail.com>
2023-01-18 14:36:08 +0530

    [Japneet | Shashank] Add. confirm workflow

---
+++ src/controllers/ConfirmController.js
@@ -0,0 +1,23 @@
+import log4js from 'log4js';
+import GenericResponse from '../utilities/GenericResponse';
+import ConfirmService from '../services/ConfirmService';
+import authVerifier from '../utilities/SignVerify/AuthHeaderVerifier';
+import LookUpService from '../services/LookUpService';
+
+const confirm = async (req, res) => {
+  const logger = log4js.getLogger('ConfirmController');
+  logger.debug(`Confirm called with ${JSON.stringify(req.body)}`);
+  const publicKey = await LookUpService.getPublicKeyWithSubscriberId(req.body.context.bap_id);
+  authVerifier.authorize(req, publicKey).then(() => {
+    logger.debug('Request Authorized Successfully.');
+    ConfirmService.confirm(req.body);
+    GenericResponse.sendAcknowledgement(res);
+  }).catch((err) => {
+    logger.debug(`Authorization Failed: ${err}`);
+    GenericResponse.sendErrorWithAuthorization(res);
+  });
+};
+
+export default {
+  confirm,
+};

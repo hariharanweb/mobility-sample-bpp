58dc85404c849efbc160c262cf151abe35861d87
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2023-02-20 16:08:21 +0530

    [Hari] Removing logging noise

--- src/utilities/Cache.js
+++ src/utilities/Cache.js
@@ -1,17 +1,11 @@
 import NodeCache from 'node-cache';
-import LoggingService from '../services/LoggingService';
 
 const cache = new NodeCache({ stdTTL: 120, checkperiod: 200000 });
-const logger = LoggingService.getLogger('Cache');
 
-const getCache = (cachekey) => {
-  logger.debug(`Inside get chache, the public key is: ${cache.get(cachekey)}`);
-  return cache.get(cachekey);
-};
+const getCache = (cachekey) => cache.get(cachekey);
 
 const setCache = (cachekey, cacheValue, ttl) => {
   cache.set(cachekey, cacheValue, ttl);
-  logger.debug(`Inside set chache, the public key is: ${cache.get(cachekey)}`);
 };
 
 export default {

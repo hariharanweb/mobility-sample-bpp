00f2a52ea4a8e3e6daeb48a0ea60ccb066cb2082
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2023-03-02 11:53:54 +0530

    [Hari] Removing unnecessary logs

--- src/utilities/SignVerify/SignatureHelper.js
+++ src/utilities/SignVerify/SignatureHelper.js
@@ -11,7 +11,6 @@ const getCreatedAndExpires = () => {
   const expires = (
     parseInt(created, 10) + 1 * 60 * 60).toString();
 
-  logger.debug(`Created at ${created}, Expires at ${expires}`);
   return [created, expires];
 };
 
@@ -24,9 +23,7 @@ const createSignedData = (data, privateKey) => {
     data,
     sodium.from_base64(privateKey, sodium.base64_variants.ORIGINAL),
   );
-  const signedMessageBase64 = sodium.to_base64(signedMessage, sodium.base64_variants.ORIGINAL);
-  logger.debug(`Signed Message: ${signedMessageBase64}`);
-  return signedMessageBase64;
+  return sodium.to_base64(signedMessage, sodium.base64_variants.ORIGINAL);
 };
 
 const createSignature = async (body, createdAndExpiresValue, privateKey) => {
@@ -38,8 +35,6 @@ const createSignature = async (body, createdAndExpiresValue, privateKey) => {
     createdAndExpiresValue[1],
     digestBase64,
   );
-  logger.debug(`Digest Base 64: ${digestBase64}`);
-  logger.debug(`Signing String : ${signingString}`);
   return createSignedData(signingString, privateKey);
 };
 
@@ -56,7 +51,6 @@ const getSignature = (headers) => {
   const signaturePart = AuthHeaderSplitter.splitAuthHeader(
     `${headers['x-gateway-authorization']}`,
   );
-  logger.debug(signaturePart.signature);
   return signaturePart.signature;
 };
 
@@ -64,7 +58,6 @@ const getCreated = (headers) => {
   const signaturePart = AuthHeaderSplitter.splitAuthHeader(
     headers['x-gateway-authorization'],
   );
-  logger.debug(signaturePart.created);
   return signaturePart.created;
 };
 
@@ -72,7 +65,6 @@ const getExpires = (headers) => {
   const signaturePart = AuthHeaderSplitter.splitAuthHeader(
     headers['x-gateway-authorization'],
   );
-  logger.debug(signaturePart.expires);
   return signaturePart.expires;
 };
 
@@ -83,15 +75,11 @@ const createSigningStringUsingTime = async (body, created, expires) => {
     digest,
     sodium.base64_variants.ORIGINAL,
   );
-  logger.debug(`Digest Base 64: ${digestBase64}`);
-  const signingString = concatinateSignature(
+  return concatinateSignature(
     created,
     expires,
     digestBase64,
   );
-  logger.debug(`Signing String : ${signingString}`);
-
-  return signingString;
 };
 
 export default {
